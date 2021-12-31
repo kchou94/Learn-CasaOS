731d55e6d39624a81399e63769828eb8754388c5
Berny Linville <bernylinville@devopsthink.org>
2021-12-31 10:52:09 +0800

    add v1.Zerotier

--- pkg/utils/httper/httper.go
+++ pkg/utils/httper/httper.go
@@ -2,7 +2,9 @@ package httper
 
 import (
 	"bytes"
+	"encoding/json"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"time"
 )
@@ -42,5 +44,34 @@ func Get(url string, head map[string]string) (response string) {
 		}
 	}
 	response = result.String()
+	return
+}
+
+// 发送POST请求
+// url:请求地址，data:POST请求提交的数据,contentType:请求体格式，如：application/json
+// content:请求放回的内容
+func ZeroTierPost(url string, data map[string]string, head map[string]string, cookies []*http.Cookie) (content string, code int) {
+	b, _ := json.Marshal(data)
+	req, err := http.NewRequest("POST", url, bytes.NewReader(b))
+	for _, cookie := range cookies {
+		req.AddCookie(cookie)
+	}
+	for k, v := range head {
+		req.Header.Add(k, v)
+	}
+	if err != nil {
+		panic(err)
+	}
+
+	client := &http.Client{Timeout: 20 * time.Second}
+	resp, error := client.Do(req)
+
+	if error != nil {
+		panic(error)
+	}
+	defer resp.Body.Close()
+	code = resp.StatusCode
+	result, _ := ioutil.ReadAll(resp.Body)
+	content = string(result)
 	return
 }
